3d0465276f261b8dd9259b8e7be300a38d5611e7
Prashant mohite <113103569+PrashantMohite1@users.noreply.github.com>
2023-11-18 14:25:01 +0530

    added terraform oci instance creation scripts

---
+++ Terraform-OCI/OCI Instance creation/variables.tf
@@ -0,0 +1,105 @@
+variable region { default = "us-phoenix-1" }
+variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaagkbzgg6lpzrf47xzy4rjoxg4de6ncfiq2rncmjiujvy2hjgxvziq" }
+variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaagkbzgg6lpzrf47xzy4rjoxg4de6ncfiq2rncmjiujvy2hjgxvziq" }
+
+
+variable "tenancy_ocid" {
+    default = "ocid1.tenancy.oc1..aaaaaaaagkbzgg6lpzrf47xzy4rjoxg4de6ncfiq2rncmjiujvy2hjgxvziq"
+}
+
+variable "user_ocid" {
+
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "region" {
+    default = "us-phoenix-1"
+}
+
+variable "compartment_ocid" {
+    default = "ocid1.tenancy.oc1..aaaaaaaagkbzgg6lpzrf47xzy4rjoxg4de6ncfiq2rncmjiujvy2hjgxvziq"
+}
+
+variable "ssh_public_key" {
+}
+
+variable "ssh_private_key" {
+}
+
+variable "kms_key_ocid" {
+}
+
+
+# instance related variables
+# Defines the number of instances to deploy
+variable "num_instances" {
+  default = "3"
+}
+
+# Defines the number of volumes to create and attach to each instance
+# NOTE: Changing this value after applying it could result in re-attaching existing volumes to different instances.
+# This is a result of using 'count' variables to specify the volume and instance IDs for the volume attachment resource.
+variable "num_iscsi_volumes_per_instance" {
+  default = "1"
+}
+
+variable "num_paravirtualized_volumes_per_instance" {
+  default = "2"
+}
+
+variable "instance_shape" {
+  default = "VM.Standard.E3.Flex"
+}
+
+variable "instance_ocpus" {
+  default = 1
+}
+
+variable "instance_vcpus" {
+  default = 2
+}
+
+variable "instance_shape_config_memory_in_gbs" {
+  default = 1
+}
+
+variable "instance_image_ocid" {
+  type = map(string)
+
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Oracle-Linux-7.5-2018.10.16-0"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaoqj42sokaoh42l76wsyhn3k2beuntrh5maj3gmgmzeyr55zzrwwa"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaitzn6tdyjer7jl34h2ujz74jwy5nkbukbh55ekp6oyzwrtfa4zma"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaa32voyikkkzfxyo4xbdmadc2dmvorfxxgdhpnk6dw64fa3l4jh7wa"
+  }
+}
+
+variable "flex_instance_image_ocid" {
+  type = map(string)
+  default = {
+    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaa6hooptnlbfwr5lwemqjbu3uqidntrlhnt45yihfj222zahe7p3wq"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaa6tp7lhyrcokdtf7vrbmxyp2pctgg4uxvt4jz4vc47qoc2ec4anha"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaadvi77prh3vjijhwe5xbd6kjg3n5ndxjcpod6om6qaiqeu3csof7a"
+    uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaaw5gvriwzjhzt2tnylrfnpanz5ndztyrv3zpwhlzxdbkqsjfkwxaq"
+  }
+}
+
+variable "db_size" {
+  default = "50" # size in GBs
+}
+
+variable "tag_namespace_description" {
+  default = "Just a test"
+}
+
+variable "tag_namespace_name" {
+  default = "testexamples-tag-namespace"
+}
+
